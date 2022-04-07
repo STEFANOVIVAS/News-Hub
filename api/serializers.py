3e6f0945bb82f6c23e431c5a63a206d64d214635
Stefano Vivas <stefanovivas@gmail.com>
2022-04-07 14:41:22 -0300

    DRF filter and search implemented. Issue #4 fixed.

--- api/serializers.py
+++ api/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 
 class NoticiaSerializer(serializers.ModelSerializer):
+    data_noticia = serializers.DateField(input_formats=['%d/%m/%Y'], format='%d/%m/%Y',style={'placeholder': 'Data noticia', 'autofocus': True})
     class Meta:
         model = Noticia
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,filters
 from api.serializers import NoticiaSerializer
 from noticias.models import Noticia
 from rest_framework.views import APIView
+import django_filters 
+
 
 # Create your views here.
 
+CHOICES=(('Uol','Uol'),('Ebc','Ebc'),('Portal_G1','Portal G1'),)
+class NoticiasFilter(django_filters.FilterSet):
+    
+    fonte = django_filters.MultipleChoiceFilter(choices = CHOICES)
+
+    class Meta:
+        model = Noticia
+        fields ={'fonte':['exact'], 
+        'data_noticia':['lte','gte'],}
+        
 
 class NoticiasListView(generics.ListAPIView):
     queryset=Noticia.objects.all()
     serializer_class = NoticiaSerializer
+    
+    filterset_class=NoticiasFilter
+    search_fields=['titulo','conteudo']
 
 
 class NoticiasDetailView(generics.RetrieveAPIView):
@@ -17,15 +32,15 @@ class NoticiasDetailView(generics.RetrieveAPIView):
     serializer_class = NoticiaSerializer
 
 
-class NoticiaFonteView(generics.ListAPIView):
-    serializer_class=NoticiaSerializer
+# class NoticiaFonteView(generics.ListAPIView):
+#     serializer_class=NoticiaSerializer
 
-    def get_queryset(self):
-        queryset=Noticia.objects.all()
-        fonte=self.kwargs['fonte']
-        if fonte:
-            queryset=Noticia.objects.filter(fonte=fonte)
-        return queryset
+#     def get_queryset(self):
+#         queryset=Noticia.objects.all()
+#         fonte=self.kwargs['fonte']
+#         if fonte:
+#             queryset=Noticia.objects.filter(fonte=fonte)
+#         return queryset
 
        
         
